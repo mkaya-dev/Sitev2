b4b41a3a78a8817329f94a823f0212336dbc1a38
Mustafa Kaya <123100753+mkaya-dev@users.noreply.github.com>
2024-02-25 17:59:46 +0300

    Update Navbar.js

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -4,39 +4,19 @@ import { useNavigate } from 'react-router-dom'
 
 
 const Navbar = () => {
-    const GaleriYon=useNavigate()
-    const Kayıt=useNavigate()
-
-    const Docs=()=>{
-        window.location="/Docs"
-    }
-
-    const Tıkla=()=>{
-        window.location="/Ricky"
-    }
-
-    const login=useNavigate()
-
-   
-
-    const Home=useNavigate()
-
-
+    const navigate = useNavigate()
     return (
         <>
          <nav className='arkaplanNav'>
             <h3 style={{cursor:"pointer"}} onClick={()=>Home("/")} className='logo'>MK.app</h3>
             <ul>
-                <li onClick={Docs}>Disnay Dökümanları</li>
-                <li onClick={Tıkla}>Rick ve Morty Bilgileri</li>
-                <li onClick={()=>GaleriYon("/Galeri")}>Galeri</li>
-                <li onClick={()=>Kayıt("/KayıtOl")}>  Kayıt Olmak</li>
-                <li><button onClick={()=>login("/KayıtOl")} className='button1 btn btn-light'>Ücretsiz Başlayın</button></li>
-
+                <li onClick={() => navigate('Docs')}>Disnay Dökümanları</li>
+                <li onClick={() => navigate('Ricky')}>Rick ve Morty Bilgileri</li>
+                <li onClick={() => navigate('/Galeri')}>Galeri</li>
+                <li onClick={() => navigate('/KayitOl')}>  Kayıt Olmak</li>
+                <li><button onClick={() => navigate('KayitOl')} className='button1 btn btn-light'>Ücretsiz Başlayın</button></li>
             </ul>
          </nav>
-
-
         </>
     )
 }
