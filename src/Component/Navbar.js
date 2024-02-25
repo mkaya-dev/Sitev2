c6cc8c4f4307a6b8eb605c28a9ac033cbec0c13c
Mustafa Kaya <123100753+mkaya-dev@users.noreply.github.com>
2024-02-25 18:31:53 +0300

    Update Navbar.js

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -10,11 +10,11 @@ const Navbar = () => {
          <nav className='arkaplanNav'>
             <h3 style={{cursor:"pointer"}} onClick={() => navigate('/')} className='logo'>MK.app</h3>
             <ul>
-                <li onClick={() => navigate('./Docs')}>Disnay Dökümanları</li>
-                <li onClick={() => navigate('./Ricky')}>Rick ve Morty Bilgileri</li>
-                <li onClick={() => navigate('./Galeri')}>Galeri</li>
-                <li onClick={() => navigate('./KayitOl')}>  Kayıt Olmak</li>
-                <li><button onClick={() => navigate('./KayitOl')} className='button1 btn btn-light'>Ücretsiz Başlayın</button></li>
+                <li onClick={() => navigate('/Docs', { replace: true })}>Disnay Dökümanları</li>
+                <li onClick={() => navigate('/Ricky', { replace: true })}>Rick ve Morty Bilgileri</li>
+                <li onClick={() => navigate('/Galeri', { replace: true })}>Galeri</li>
+                <li onClick={() => navigate('/KayitOl', { replace: true })}>  Kayıt Olmak</li>
+                <li><button onClick={() => navigate('/KayitOl', { replace: true })} className='button1 btn btn-light'>Ücretsiz Başlayın</button></li>
             </ul>
          </nav>
         </>
